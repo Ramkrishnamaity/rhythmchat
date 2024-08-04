4ac5e042e7a66c0abb81ba3d41a107ab2a82e25d
Ramkrishna Maity <ramkrishnamaity4146@gmail.com>
2024-08-04 18:22:24 +0530

    Worked on Auth Flow

--- src/components/common/ConfirmationModal.tsx
+++ src/components/common/ConfirmationModal.tsx
@@ -13,7 +13,7 @@ const ConfirmationModal: React.FC<ModalPropsType> = ({ desc, btnText, triggerFun
 
   return (
     <div className='z-10 lg:rounded-3xl rounded-none fixed overflow-auto top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-opacity-10 backdrop-blur-sm'>
-      <div className='text-black border border-2 border-[white] flex flex-col items-center gap-1 md:p-8 p-5 rounded-lg bg-wrapper '>
+      <div className='text-black border-2 border-[white] flex flex-col items-center gap-1 md:p-8 p-5 rounded-lg bg-wrapper '>
         <h1>Are you sure?</h1>
         <p>{desc}</p>
         <div className='my-2'
